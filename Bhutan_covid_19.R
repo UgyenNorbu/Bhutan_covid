422c533aafe92b6d4216ce843cb9a1ef114f7a31
Ugyen Norbu <unorbu2007@gmail.com>
2020-06-16 21:44:51 +0600

    Wiki changed table format.

--- Bhutan_covid_19.R
+++ Bhutan_covid_19.R
@@ -4,7 +4,6 @@ library(janitor)
 library(scales)
 library(ggrepel)
 
-
 wiki_pg <- "https://en.wikipedia.org/wiki/COVID-19_pandemic_in_Bhutan#cite_note-MoH-23May-43"
 
 covid <- read_html(wiki_pg)
@@ -13,46 +12,40 @@ covid_table <- covid %>%
     html_table(fill = TRUE)
 
 covid_Bhutan <- covid_table[[3]]
+colnames(covid_Bhutan) <-c("Date", "total_cases", "new_case", "Admitted_iso", "Admitted_di_iso", "Recvrd", "Deaths", "Quar_in", "Quar_disch", "Tested_tot","Tested_PCR", "Ref")
+
+covid_Bhutan <- covid_Bhutan[-1, ]
+
 covid_Bhutan <- covid_Bhutan %>% 
     clean_names() %>% 
-    select(-c(caseno, ref)) %>% 
-    mutate(date = as.Date(date, format("%B %d")))
+    mutate(date = as.Date(date, format("%B %d"))) %>% 
+    mutate(total_cases =parse_number(total_cases)) %>% 
+    mutate(new_case = parse_number(new_case)) %>% 
+    mutate(admitted_iso = parse_number(admitted_iso)) %>% 
+    mutate(admitted_di_iso = parse_number(admitted_di_iso)) %>% 
+    mutate(recvrd = parse_number(recvrd)) %>% 
+    mutate(deaths = parse_number(deaths)) %>% 
+    mutate(quar_in = parse_number(quar_in)) %>% 
+    mutate(quar_disch = parse_number(quar_disch)) %>% 
+    mutate(tested_tot = parse_number(tested_tot)) %>% 
+    mutate(tested_pcr = parse_number(tested_pcr)) %>% 
+    select(-ref)
+    
+write_csv(covid_Bhutan, "output_data/covid_Bhutan.csv")
 
-write_csv(covid_Bhutan, "covid_Bhutan.csv")
-
-covid_grouped <- covid_Bhutan %>%
-    group_by(date) %>% 
-    summarise(new_cases = n()) 
-
-covid_grouped$total_cases <- cumsum(covid_grouped$new_cases)
-
-covid_grouped_long <- pivot_longer(covid_grouped, -date, 
-             names_to = "case_type", values_to = "number")
-
-covid_summary <- covid_Bhutan %>% 
-    group_by(status) %>% 
-    count()
-
-covid_active_case <- covid_summary %>% 
-    filter(status == "Admitted") %>% 
-    select(n) %>% 
-    pull()
-
-covid_recoverd_case <- covid_summary %>% 
-    filter(status == "Recovered") %>% 
-    select(n) %>% 
-    pull()
-
-my_subtitle <- paste(paste("Active cases =", covid_active_case, sep = " "), 
-                     paste("Recoverd =", covid_recoverd_case, sep = " "),
-                     paste("Total cases =", sum(covid_grouped$new_cases)),
+my_subtitle <- paste(paste("New case(s) =", 
+                           covid_Bhutan$new_case[nrow(covid_Bhutan)]), 
+                     paste("Recoverd =", 
+                           covid_Bhutan$recvrd[nrow(covid_Bhutan)]),
+                     paste("Total cases =", 
+                           covid_Bhutan$total_cases[nrow(covid_Bhutan)]),
                      sep = "    ")
 
-covid_grouped_long %>% 
-    mutate(label = ifelse(date == max(date), covid_grouped_long$number, NA_character_)) %>% 
-    ggplot(aes(x = date, y = number, group = case_type)) +
-    geom_line(alpha = 0.2, size = 1.5) +
-    geom_point(aes(colour = case_type), size = 3) +
+covid_Bhutan %>% 
+    ggplot(aes(x = date)) +
+    geom_line(aes(y = total_cases, color = "Total cases")) +
+    geom_line(aes(y = new_case, color = "New case")) +
+    geom_line(aes(y = recvrd, color = "Recovered")) +
     scale_x_date(date_breaks = "1 week", date_labels = "%d-%B") +
     labs(x = "Date",
          y = "Number of patients",
@@ -60,12 +53,10 @@ covid_grouped_long %>%
          subtitle = my_subtitle,
          caption = "Data source: https://en.wikipedia.org/wiki/COVID-19_pandemic_in_Bhutan#cite_note-MoH-23May-43",
          colour = "Case type") +
-    scale_color_discrete(labels = c("New cases", "Total cases")) +
-    geom_label_repel(aes(label = label),nudge_x = 1.5, segment.size = 0.0, 
-                    label.size = 0.3) +
-    theme_minimal() +
+    theme_light() +
     theme(axis.text.x = element_text(angle = 15),
-          plot.title = element_text(hjust = 0.5),
+          plot.title = element_text(face = "bold", hjust = 0.5),
           plot.subtitle = element_text(hjust = 0.5),
           legend.position = c(0.8, 0.9))
-ggsave("covid_19_cases_in_Bhutan.jpg", width = 25, height = 15, units = "cm")
+
+ggsave("output_viz/Covid_19_cases_in_Bhutan.jpg", width = 25, height = 15, units = "cm")
